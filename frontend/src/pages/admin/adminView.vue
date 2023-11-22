78b727475527cc5c4e976f3941d009494809aaeb
kimsungyoun <kimsy311@hanmail.net>
2023-11-22 16:06:24 +0900

    [2023-11-22 16:06] add previous, next button

--- frontend/src/pages/admin/adminView.vue
+++ frontend/src/pages/admin/adminView.vue
@@ -13,19 +13,21 @@
                         <td>처리상태</td>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr v-for="(i, idx) in state.items1" :key="idx">
-                        <td v-if="i.status == '진행중'">{{ i.no }}</td>
-                        <td v-if="i.status == '진행중'">{{ i.employee.empname }}</td>
-                        <td v-if="i.status == '진행중'">{{ lib.formattedTime2(i.day) }}</td>
-                        <td v-if="i.status == '진행중'">{{ i.content }}</td>
-                        <td v-if="i.status == '진행중'"><a>{{ i.status }}</a></td>
+                <tbody v-for="(i, idx) in state.items1" :key="idx">
+                    <tr v-if="i.status == '진행중'">
+                        <td><a @click="process(i.no)">{{ i.no }}</a></td>
+                        <td>{{ i.employee.empname }}</td>
+                        <td>{{ lib.formattedTime2(i.day) }}</td>
+                        <td>{{ i.content }}</td>
+                        <td>{{ i.status }}</td>
                     </tr>
                 </tbody>
             </table>
         </div>
         <div class="page" v-if="state.totalPages1">        
+            <input type="button" @click="previous1()" value="이전"/>
             <input type="button" v-for="pageNumber in state.totalPages1" :key="pageNumber" :value="pageNumber" @click="changePage1(pageNumber)"/>
+            <input type="button" @click="next1()" value="다음"/>
         </div>
     </div>
     <div class="doneRequest-list">
@@ -47,13 +49,15 @@
                         <td>{{ i.employee.empname  }}</td>
                         <td>{{ lib.formattedTime2(i.day) }}</td>
                         <td>{{ i.content }}</td>
-                        <td><a>{{ i.status }}</a></td>
+                        <td>{{ i.status }}</td>
                     </tr>
                 </tbody>
             </table>
         </div>
-        <div class="page" v-if="state.totalPages2">        
+        <div class="page" v-if="state.totalPages2">    
+            <input type="button" @click="previous2()" value="이전"/>    
             <input type="button" v-for="pageNumber in state.totalPages2" :key="pageNumber" :value="pageNumber" @click="changePage2(pageNumber)"/>
+            <input type="button" @click="next2()" value="다음"/>
         </div>
     </div>
 </div>    
@@ -91,7 +95,19 @@ const changePage1 = (pageNumber) => {
   state.currentPage1 = pageNumber-1;
   fetchData1();
 };
+const previous1 = () =>{
+    if(state.currentPage1 != 0){
+        state.currentPage1 = state.currentPage1 - 1;
+        fetchData1();
+    }
+}
 
+const next1 =()=>{
+    if(state.totalPages1 - 1 > state.currentPage1){
+        state.currentPage1 = state.currentPage1 + 1;
+        fetchData1();
+    }
+}
 // 처리 목록
 const fetchData2=()=>{
     const pageSize = 5;
@@ -106,6 +122,23 @@ const changePage2 = (pageNumber) => {
   state.currentPage2 = pageNumber-1;
   fetchData2();
 };
+const previous2 = () =>{
+    if(state.currentPage2 != 0){
+        state.currentPage2 = state.currentPage2 - 1;
+        fetchData2();
+    }
+}
+
+const next2 =()=>{
+    if(state.totalPages2 - 1 > state.currentPage2){
+        state.currentPage2 = state.currentPage2 + 1;
+        fetchData2();
+    }
+}
+// 요청 처리
+const process=(rno)=>{
+    alert("요청 번호 : "+rno);
+}
 
 onMounted(()=>{
     load();
@@ -124,16 +157,23 @@ onMounted(()=>{
     border-radius: 20px;
 }
 .page{
-    text-align: center;
-    margin: 10px 0;
+    display: flex;
+    flex-direction: row;
+    margin-top: 10px;
+    justify-content: center;
+}
+input[type=button]{
+    margin-right: 5px;
 }
 table{
     border-spacing: 0;
     text-align: center;
     width: 100%;
 }
-
 tr, td {
     border: 1px solid #98abdf;
+}
+a:hover{
+    cursor: pointer;
 }
 </style>
